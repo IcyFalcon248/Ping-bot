c200bebc2d074ad55a6ea2895de4271a1e7ec264
IcyFalcon248 <87845850+IcyFalcon248@users.noreply.github.com>
2024-10-01 10:05:43 +0530

    Update ping-bot.js

--- ping-bot.js
+++ ping-bot.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const https = require('https'); // Change to https
 
 // The URL of your Replit bot
 const replitBotUrl = 'https://9b5dcba5-4010-4773-9b9b-02aab0d6bd46-00-1bbpjra9g180o.pike.replit.dev:3000/'; // Replace with your Replit URL
 
 setInterval(() => {
-    http.get(replitBotUrl, (res) => {
+    https.get(replitBotUrl, (res) => { // Use https.get instead of http.get
         console.log(`Pinged Replit bot: ${res.statusCode}`);
     }).on('error', (err) => {
         console.error(`Error pinging Replit bot: ${err.message}`);
@@ -12,6 +12,7 @@ setInterval(() => {
 }, 1000); // Ping every second
 
 // Start a simple HTTP server
+const http = require('http');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Ping bot is running!\n');
